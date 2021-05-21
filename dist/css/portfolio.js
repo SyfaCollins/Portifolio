3d55b58cd1908ef0bcb2c44d8aeb5725aaa665b7
SyfaCollins <coniplexy@gmail.com>
2021-05-22 01:48:04 +0300

    added javascript file

---
+++ dist/css/portfolio.js
@@ -0,0 +1,24 @@
+let pop_up = document.querySelector("#pop_up")
+let hire_me = document.getElementById('hire_me')
+
+let close_button = document.getElementById('close_button')
+
+
+
+// function open_button(){
+//     pop_up.style.visibility = 'visible'
+// }
+
+
+function close_button(){
+    pop_up.style.visibility = 'hidden'
+  
+
+}
+
+// hire_me.addEventListener('mousedown',open_button)
+
+close_button.addEventListener('click',close_button)
+
+// close_button.addEventListener('click',do)
+
