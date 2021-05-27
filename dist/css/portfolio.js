b9e7b869473af480ed18bb541ad32d8c28434823
SyfaCollins <coniplexy@gmail.com>
2021-05-27 15:18:50 +0300

    added mentorship page

--- dist/css/portfolio.js
+++ dist/css/portfolio.js
@@ -4,21 +4,23 @@ let hire_me = document.getElementById('hire_me')
 let close_button = document.getElementById('close_button')
 
 
+function show(){
+    pop_up.style.display = 'flex'
+    console.log('i was clicked')
+    pop_up.style.transition= '5s 1s cubic-bezier(1, 0, 0, 1)'
 
-// function open_button(){
-//     pop_up.style.visibility = 'visible'
-// }
-
-
-function close_button(){
-    pop_up.style.visibility = 'hidden'
-  
 
 }
 
-// hire_me.addEventListener('mousedown',open_button)
+function hide(){
+    pop_up.style.display = 'none'
+    console.log('i was clicked')
 
-close_button.addEventListener('click',close_button)
+}
 
-// close_button.addEventListener('click',do)
+hire_me.addEventListener('click',show)
+
+close_button.addEventListener('click',hide)
+
+
 
